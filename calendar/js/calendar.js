632916fa42619ff9b4956bb8b99975d939347e95
luan293 <luan29394@gmail.com>
2016-12-09 10:02:31 +0700

    push cld

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -72,48 +72,63 @@ function checkY() {
 function cleartable() {
   var x = document.getElementsByTagName("td");
   var y = document.getElementsByTagName("td").length;
-  for(var i=0; i<y; i++) {
+  for(var i=0; i < y; i++) {
 	  x[i].innerHTML = " ";
 	  x[i].className = "";
 	}
 }
 
 function printtable(year, month) {
-  cleartable();
-  var year = year;
-  var month = month;
-  var day = new Array();
-  day[0]="";
-  var d = new Date(year,month-1,1); 
-  var fristdayofmonth = d.getDay(); 
-  if(checkY() == true) {
-   	for(var i=0; i<fristdayofmonth; i++){
-   		//thang trc
-   		day[i]="";
-	   	day[i] += " ";
-	   	//count++;
-	  }
-	  //var count5 = count
-	  for(var i = 1; i <= thangngaynhuan[month-1]; i++) {
-	   	day.push(i);
-	   	//count++;
-	  }
-	  var x = document.getElementsByTagName("td");
-	  //document.getElementsByTagName("td").innerHTML = "aaa";
-	  //cleartable();
-	  for(var i = 0; i < day.length; i++){
-	   	x[i].innerHTML = day[i];	   				
-	  }
-	  //alert(chia7);
-	  //	document.getElementById("tableindex").innerHTML = chia7[5];
-  }else{
+  	cleartable();
+  	var year = year;
+  	var month = month;
+  	var day = new Array();
+  	day[0]=" ";
+  	var d = new Date(year,month-1,1); 
+  	var fristdayofmonth = d.getDay(); 
+  	
+  	if(checkY() == true) {
+  		// if////////////////////////
+  		if(fristdayofmonth == 0) {
+  			alert(fristdayofmonth);
+  			day[0]="1";
+  			for(var i = 1; i < thangngaynhuan[month-1]; i++) {
+			   	day.push(i);
+			   	//count++;
+			}
+  		}else{
+  			for(var i = 0; i < fristdayofmonth; i++) {
+			   		//thang trc
+			   		day[i]="";
+				   	day[i] += " ";
+				   	//count++; 	
+				   //alert(day[i]);
+  			}
+	  			for(var i = 1; i <= thangngaynhuan[month-1]; i++) {
+			   	day.push(i);
+			   	//count++;
+			}
+  		}
+  			
+		  //var count5 = count
+
+		
+		var x = document.getElementsByTagName("td");
+		//document.getElementsByTagName("td").innerHTML = "aaa";
+		//cleartable();
+		for(var i = 0; i < day.length; i++){
+		   	x[i].innerHTML = day[i];	   				
+		}
+		//alert(chia7);
+		//	document.getElementById("tableindex").innerHTML = chia7[5];
+  	}else{
    			//nam thuong  			
    			for(var i = 0; i < fristdayofmonth; i++) {
      			//thang trc
      			day[i]="";
-  	   		day[i] += " ";
-  	   		//count++;
-	   	  }
+  	   			day[i] += " ";
+  	   			//count++;
+	   	  	}
 	   		//var count5 = count
 	   		for(var i = 1; i <= thangngay[month-1]; i++) {
 	   			day.push(i);
@@ -125,78 +140,81 @@ function printtable(year, month) {
 	   		for(var i = 0; i < day.length; i++) {
 	   			x[i].innerHTML = day[i];		
 	   		}
-    }
+    	}
   notdayofmonth();
   today();
 }
 
 function notdayofmonth() {
-  var x = document.getElementsByTagName("td");
-  for(var i = 0; i < x.length; i++){
-   	//alert(x[i].innerHTML);
-   	if(x[i].innerHTML == " ") {
-   		//alert(i);
-   		x[i].removeAttribute("onclick");
-   		x[i].className = "notdayofmonth";
-   	}		
+  	var x = document.getElementsByTagName("td");
+  	for(var i = 0; i < x.length; i++){
+   		//alert(x[i].innerHTML);
+   		if(x[i].innerHTML == " ") {
+	   		//alert(i);
+	   		console.log("notday")
+	   		x[i].removeAttribute("onclick");
+	   		x[i].className = "notdayofmonth";
+   		}else{
+   			x[i].className = "dayofmonth";
+   		}		
 	}
 }
 
 function today() {
-  var a = new Date();
+  	var a = new Date();
 	var b = a.getDate();
 	var c = a.getFullYear();
 	var d = a.getMonth()+1;
-  var x = document.getElementsByTagName("td");
-  if(c == getY() && d == getM()) {
-	  for(var i = 0; i < x.length; i++) {
-	   	//alert(x[i].innerHTML);
-	   	if(x[i].innerHTML == b){
-	   		//alert(i);
-	   				x[i].className = "today";
-	   	}		
-	  }
-  }else{
+  	var x = document.getElementsByTagName("td");
+  	if(c == getY() && d == getM()) {
+	  	for(var i = 0; i < x.length; i++) {
+	   		//alert(x[i].innerHTML);
+		   	if(x[i].innerHTML == b){
+		   		//alert(i);
+		   		x[i].className = "today";
+		   	}		
+	  	}
+  	}else{
 
-   		 }
+   		}
 }
 
 function btncluinam() {
-  var a = getY()-1; 
-  if(a < minnam) {
-   	alert("lui it thoi");
-  }else{
+  	var a = getY()-1; 
+  	if(a < minnam) {
+   		alert("lui it thoi");
+  	}else{
    			SelectY(a);
    			printtable(a,getM()); 			
    		}
 }
 
 function btnctiennam() {
-  var a = getY()+1;
-  if(a > maxmam){
-    alert("tien it thoi");
-  }else{
+  	var a = getY()+1;
+  	if(a > maxmam){
+    	alert("tien it thoi");
+  	}else{
    			SelectY(a);
    			printtable(a,getM());  			
    		}	
 }
 
-function btnluithang(){
-  var a = parseInt(getM())-1;
-  //alert("lui thang"+a);
-  if(a < 1) {
-   	alert("lui it thoi");
-  }else{
+function btnluithang() {
+  	var a = parseInt(getM())-1;
+  	//alert("lui thang"+a);
+  	if(a < 1) {
+   		alert("lui it thoi");
+  	}else{
    			SelectM(a);
    			printtable(getY(),a);  			
    		}	  		
 }
 
-function btntienthang(){
-  var a = parseInt(getM())+1;
-  if(a > 12){
-   	alert("tien it thoi");
-  }else{
+function btntienthang() {
+  	var a = parseInt(getM())+1;
+  	if(a > 12){
+   		alert("tien it thoi");
+  	}else{
    			SelectM(a);
    			printtable(getY(),a);  			
    		}	
@@ -217,6 +235,8 @@ function pickup(day) {
 	var year = getY();
 	var pick = day+"/"+month+"/"+year;
 	document.getElementById("textbox-date").value = pick;
+	document.getElementById("showcdl").style.display = "none";
+	showcdl();
 }
 
 function showcdl() {
