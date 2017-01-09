f0ef1d5e68899160247e12fb3eda77ac4f122609
luan293 <luan29394@gmail.com>
2017-01-09 18:15:28 +0700

    push

--- webrespon/js/js.js
+++ webrespon/js/js.js
@@ -27,29 +27,26 @@ function menu(){
 	if($( window ).width() > 644){
 		menuOnpc();
 	}else{
-
-		$(".mean-expand").on("tap",function(){
-
-			console.log("aaaaaa");
-		});
+		
+		
 		var menumb = $("#main_menu").html();
 		$("#menu_mb").html(menumb);
 		for(var i = 0; i<count_pmenu; i++){	
 		var a = $(".menu1").eq(i);
 		if(a.children('ul').length > 0){
-			$(a.children('ul')).after('<a class="mean-expand" id="mean-expand1">+</a>');
+			$(a.children('ul')).after('<a class="mean-expand" id="mean-expand'+i+'">+</a>');
 			}
 		}
-		//menuOnmb();
+		menuOnmb();
 	}	
 	
 }
 function menuOnmb() {
 
-	$(".mean-expand").on("tap",function(){
-		
+  		$(".mean-expand").on("click", function(){
 		var numb = $(this).attr('id');
-		var status = $("li[.+"+numb+"]").attr('id');
+		var status = $("#"+numb).parent('li').attr('id');
+		console.log(status);
 		if(!flag){					
 			$("#"+status+" .sub_menu").css({"display": "flex"});
 			flag = true;
@@ -58,8 +55,10 @@ function menuOnmb() {
 			//$("#"+status).find("img").attr("src", "img/"+status+".jpg");
 			flag = false;
 		}
-		console.log('aa');
-	})
+		
+		});
+	
+	
 }
 function menuOnpc() {
 	$(".menu1").mouseenter(function(){
