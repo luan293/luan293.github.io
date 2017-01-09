e9b9d1f7328376442ffc236e42cc672e09b50f5c
luan293 <luan29394@gmail.com>
2017-01-09 14:57:55 +0700

    test menu mb

--- webrespon/js/js.js
+++ webrespon/js/js.js
@@ -1,3 +1,4 @@
+var flag = false;
 function load(){
 	menu();
 	slider();
@@ -19,16 +20,57 @@ function slider(){
 	
 }
 function menu(){
+	var count_cmenu;
+	var count_pmenu = ($('#single-nav>ul>li').length);
+	console.log(count_pmenu);
 	
-	$(".menu").mouseenter(function(){
+	if($( window ).width() > 644){
+		menuOnpc();
+	}else{
+
+		$(".mean-expand").on("tap",function(){
+
+			console.log("aaaaaa");
+		});
+		var menumb = $("#main_menu").html();
+		$("#menu_mb").html(menumb);
+		for(var i = 0; i<count_pmenu; i++){	
+		var a = $(".menu1").eq(i);
+		if(a.children('ul').length > 0){
+			$(a.children('ul')).after('<a class="mean-expand" id="mean-expand1">+</a>');
+			}
+		}
+		//menuOnmb();
+	}	
+	
+}
+function menuOnmb() {
+
+	$(".mean-expand").on("tap",function(){
+		
+		var numb = $(this).attr('id');
+		var status = $("li[.+"+numb+"]").attr('id');
+		if(!flag){					
+			$("#"+status+" .sub_menu").css({"display": "flex"});
+			flag = true;
+		}else{
+			$(".sub_menu").css("display", "none");
+			//$("#"+status).find("img").attr("src", "img/"+status+".jpg");
+			flag = false;
+		}
+		console.log('aa');
+	})
+}
+function menuOnpc() {
+	$(".menu1").mouseenter(function(){
 		var status = $(this).attr('id');		
 		$("#"+status).find("img").attr("src", "img/"+status+"_hover.jpg");
-		$("#"+status+" .sub_menu").css({"display": "block", "width": "100%"});
+		$("#"+status+" .sub_menu").css({"display": "flex"});
 		//$("#"+status+" ul").css("z-index", "10");
-	})
-	$(".menu").mouseleave(function(){
-		var status = $(this).attr('id');
-		$(".sub_menu").css("display", "none");
-		$("#"+status).find("img").attr("src", "img/"+status+".jpg");
-	})
+		})
+		$(".menu1").mouseleave(function(){
+			var status = $(this).attr('id');
+			$(".sub_menu").css("display", "none");
+			$("#"+status).find("img").attr("src", "img/"+status+".jpg");
+		})
 }
